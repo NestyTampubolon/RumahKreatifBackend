b719fc79f1949e626897ee8463db3daaba6fe88a
John Ryan Siallagan <jr.siallagan@gmail.com>
2022-09-01 08:11:15 +0700

    update john 01/09/2022

---
+++ resources/views/user/layout/header.blade.php
@@ -0,0 +1,230 @@
+<style>
+    .header-intro-clearance .header-bottom .container::before{
+        visibility: hidden;
+    }
+
+    .header-intro-clearance .header-bottom .container::after{
+        visibility: hidden;
+    }
+</style>
+
+<header class="header header-2 header-intro-clearance">
+    <div class="header-middle">
+        <div class="container">
+            <div class="header-left">
+                <button class="mobile-menu-toggler">
+                    <span class="sr-only">Toggle mobile menu</span>
+                    <i class="icon-bars"></i>
+                </button>
+                
+                <a href="index.html" class="logo">
+                    <img src="{{ URL::asset('asset/Molla/assets/images/demos/demo-2/logo.png') }}" alt="Molla Logo" width="105" height="25">
+                </a>
+            </div><!-- End .header-left -->
+
+            <div class="header-center">
+                <div class="header-search header-search-extended header-search-visible header-search-no-radius d-none d-lg-block">
+                    <a href="#" class="search-toggle" role="button"><i class="icon-search"></i></a>
+                    <form action="#" method="get">
+                        <div class="header-search-wrapper search-wrapper-wide">
+                            <label for="q" class="sr-only">Search</label>
+                            <input type="search" class="form-control" name="q" id="q" placeholder="Cari Produk ..." required>
+                            <button class="btn btn-primary" type="submit"><i class="icon-search"></i></button>
+                        </div><!-- End .header-search-wrapper -->
+                    </form>
+                </div><!-- End .header-search -->
+            </div>
+
+            <div class="header-right">
+                <div class="wishlist">
+                    <a href="wishlist.html" title="Wishlist">
+                        <div class="icon">
+                            <i class="icon-heart-o"></i>
+                            <!-- <span class="wishlist-count badge">3</span> -->
+                        </div>
+                        <p>Daftar Keinginan</p>
+                    </a>
+                </div><!-- End .compare-dropdown -->
+
+                <div class="dropdown cart-dropdown">
+                    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
+                        <div class="icon">
+                            <i class="icon-shopping-cart"></i>
+                            <!-- <span class="cart-count">2</span> -->
+                        </div>
+                        <p>Keranjang</p>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <div class="dropdown-cart-products">
+                            <div class="product">
+                                <div class="product-cart-details">
+                                    <h4 class="product-title">
+                                        <a href="product.html">Beige knitted elastic runner shoes</a>
+                                    </h4>
+
+                                    <span class="cart-product-info">
+                                        <span class="cart-product-qty">1</span>
+                                        x $84.00
+                                    </span>
+                                </div><!-- End .product-cart-details -->
+
+                                <figure class="product-image-container">
+                                    <a href="product.html" class="product-image">
+                                        <img src="{{ URL::asset('asset/Molla/assets/images/products/cart/product-1.jpg') }}" alt="product">
+                                    </a>
+                                </figure>
+                                <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
+                            </div><!-- End .product -->
+
+                            <div class="product">
+                                <div class="product-cart-details">
+                                    <h4 class="product-title">
+                                        <a href="product.html">Blue utility pinafore denim dress</a>
+                                    </h4>
+
+                                    <span class="cart-product-info">
+                                        <span class="cart-product-qty">1</span>
+                                        x $76.00
+                                    </span>
+                                </div><!-- End .product-cart-details -->
+
+                                <figure class="product-image-container">
+                                    <a href="product.html" class="product-image">
+                                        <img src="{{ URL::asset('asset/Molla/assets/images/products/cart/product-2.jpg') }}" alt="product">
+                                    </a>
+                                </figure>
+                                <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
+                            </div><!-- End .product -->
+                        </div><!-- End .cart-product -->
+
+                        <div class="dropdown-cart-total">
+                            <span>Total</span>
+
+                            <span class="cart-total-price">$160.00</span>
+                        </div><!-- End .dropdown-cart-total -->
+
+                        <div class="dropdown-cart-action">
+                            <a href="cart.html" class="btn btn-primary">View Cart</a>
+                            <a href="checkout.html" class="btn btn-outline-primary-2"><span>Checkout</span><i class="icon-long-arrow-right"></i></a>
+                        </div><!-- End .dropdown-cart-total -->
+                    </div><!-- End .dropdown-menu -->
+                </div><!-- End .cart-dropdown -->
+                <div class="account">
+                    <a href="#signin-modal" data-toggle="modal" title="My account">
+                        <div class="icon">
+                            <i class="icon-user"></i>
+                        </div>
+                        <p>Akun</p>
+                    </a>
+                </div><!-- End .compare-dropdown -->
+            </div><!-- End .header-right -->
+        </div><!-- End .container -->
+    </div><!-- End .header-middle -->
+
+    <div class="header-bottom sticky-header">
+        <div class="container">
+            <!-- <div class="header-left">
+                
+            </div> -->
+
+            <div class="header-center">
+                <nav class="main-nav">
+                    <ul class="menu sf-arrows">
+                        <li class="megamenu-container active">
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="category.html" class="sf-with-ul">Shop</a>
+
+                            <div class="megamenu megamenu-md">
+                                <div class="row no-gutters">
+                                    <div class="col-md-8">
+                                        <div class="menu-col">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="menu-title">Shop with sidebar</div><!-- End .menu-title -->
+                                                    <ul>
+                                                        <li><a href="category-list.html">Shop List</a></li>
+                                                        <li><a href="category-2cols.html">Shop Grid 2 Columns</a></li>
+                                                        <li><a href="category.html">Shop Grid 3 Columns</a></li>
+                                                        <li><a href="category-4cols.html">Shop Grid 4 Columns</a></li>
+                                                        <li><a href="category-market.html"><span>Shop Market<span class="tip tip-new">New</span></span></a></li>
+                                                    </ul>
+
+                                                    <div class="menu-title">Shop no sidebar</div><!-- End .menu-title -->
+                                                    <ul>
+                                                        <li><a href="category-boxed.html"><span>Shop Boxed No Sidebar<span class="tip tip-hot">Hot</span></span></a></li>
+                                                        <li><a href="category-fullwidth.html">Shop Fullwidth No Sidebar</a></li>
+                                                    </ul>
+                                                </div><!-- End .col-md-6 -->
+
+                                                <div class="col-md-6">
+                                                    <div class="menu-title">Product Category</div><!-- End .menu-title -->
+                                                    <ul>
+                                                        <li><a href="product-category-boxed.html">Product Category Boxed</a></li>
+                                                        <li><a href="product-category-fullwidth.html"><span>Product Category Fullwidth<span class="tip tip-new">New</span></span></a></li>
+                                                    </ul>
+                                                    <div class="menu-title">Shop Pages</div><!-- End .menu-title -->
+                                                    <ul>
+                                                        <li><a href="cart.html">Cart</a></li>
+                                                        <li><a href="checkout.html">Checkout</a></li>
+                                                        <li><a href="wishlist.html">Wishlist</a></li>
+                                                        <li><a href="dashboard.html">My Account</a></li>
+                                                        <li><a href="#">Lookbook</a></li>
+                                                    </ul>
+                                                </div><!-- End .col-md-6 -->
+                                            </div><!-- End .row -->
+                                        </div><!-- End .menu-col -->
+                                    </div><!-- End .col-md-8 -->
+
+                                    <div class="col-md-4">
+                                        <div class="banner banner-overlay">
+                                            <a href="category.html" class="banner banner-menu">
+                                                <img src="{{ URL::asset('asset/Molla/assets/images/menu/banner-1.jpg') }}" alt="Banner">
+
+                                                <div class="banner-content banner-content-top">
+                                                    <div class="banner-title text-white">Last <br>Chance<br><span><strong>Sale</strong></span></div><!-- End .banner-title -->
+                                                </div><!-- End .banner-content -->
+                                            </a>
+                                        </div><!-- End .banner banner-overlay -->
+                                    </div><!-- End .col-md-4 -->
+                                </div><!-- End .row -->
+                            </div><!-- End .megamenu megamenu-md -->
+                        </li>
+                        <li>
+                            <a href="#" class="sf-with-ul">Pages</a>
+
+                            <ul>
+                                <li>
+                                    <a href="about.html" class="sf-with-ul">About</a>
+
+                                    <ul>
+                                        <li><a href="about.html">About 01</a></li>
+                                        <li><a href="about-2.html">About 02</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html" class="sf-with-ul">Contact</a>
+
+                                    <ul>
+                                        <li><a href="contact.html">Contact 01</a></li>
+                                        <li><a href="contact-2.html">Contact 02</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="login.html">Login</a></li>
+                                <li><a href="faq.html">FAQs</a></li>
+                                <li><a href="404.html">Error 404</a></li>
+                                <li><a href="coming-soon.html">Coming Soon</a></li>
+                            </ul>
+                        </li>
+                    </ul><!-- End .menu -->
+                </nav><!-- End .main-nav -->
+            </div><!-- End .header-center -->
+
+            <!-- <div class="header-right">
+                
+            </div> -->
+        </div><!-- End .container -->
+    </div><!-- End .header-bottom -->
+</header><!-- End .header -->
