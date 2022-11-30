526192bffa2391491503e22a69161cba9cdf6eca
John Ryan Siallagan <jr.siallagan@gmail.com>
2022-11-30 10:55:14 +0700

    30/11/2022 updated 5

--- resources/views/user/toko/dashboard.blade.php
+++ resources/views/user/toko/dashboard.blade.php
@@ -64,13 +64,13 @@
             <div class="mb-2"></div>
             @foreach($cek_purchases as $cek_purchase)
                 @foreach($purchases as $purchase)
-                        <?php
-                            // $kurir = "";
+                    <?php
+                        $kurir = "";
 
-                            // if($purchase->courier_code == "pos"){ $kurir = "POS Indonesia (POS)"; }
-                            
-                            // else if($purchase->courier_code == "jene"){ $kurir = "Jalur Nugraha Eka (JNE)"; }
-                        ?>
+                        if($purchase->courier_code == "pos"){ $kurir = "POS Indonesia (POS)"; }
+                        
+                        else if($purchase->courier_code == "jene"){ $kurir = "Jalur Nugraha Eka (JNE)"; }
+                    ?>
                     @if($purchase->purchase_id == $cek_purchase->purchase_id)
                         @if($purchase->status_pembelian == "status2")
                             <div class="row">
@@ -78,7 +78,7 @@
                                     <div class="card card-dashboard">
                                         <div class="card-body">
                                             <h3 class="card-title">Pesanan masuk!</h3>
-                                            <p>Anda memiliki pesanan dengan kode pembelian <b>{{$purchase->kode_pembelian}}</b>. Silahkan kirim pesanan melalui <b> | {{$purchase->service}}</b>. Kemudian masukkan kirim resi pengiriman.</p>
+                                            <p>Anda memiliki pesanan dengan kode pembelian <b>{{$purchase->kode_pembelian}}</b>. Silahkan kirim pesanan melalui <b>{{$kurir}} | {{$purchase->service}}</b>. Kemudian masukkan kirim resi pengiriman.</p>
                                             <h4 style="float:right; margin-top: -5px; margin-bottom: -5px"><a href="./detail_pembelian/{{$purchase->purchase_id}}"><i class="icon-long-arrow-right"></i></a></h4>
                                         </div><!-- End .card-body -->
                                     </div><!-- End .card-dashboard -->
@@ -151,38 +151,6 @@
                             </div><!-- End .row -->
                         @endif
                     @endif
-                <?php
-                    $count_status2 = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status2')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $count_status2_ambil = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status2_ambil')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $count_status3 = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status3')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $count_status3_ambil = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status3_ambil')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $count_status4_ambil_a = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status4_ambil_a')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $jumlah_pesanan_sedang_berlangsung = $count_status2 + $count_status2_ambil + $count_status3 + $count_status3_ambil + $count_status4_ambil_a;
-
-                    $count_status4 = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status4')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $count_status4_ambil_b = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status4_ambil_b')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $jumlah_pesanan_berhasil_belum_dibayar = $count_status4 + $count_status4_ambil_b;
-
-                    $count_status5 = DB::table('product_purchases')->where('merchant_id', $toko)->where('is_cancelled', 0)->where('status_pembelian', 'status5')
-                    ->join('purchases', 'product_purchases.purchase_id', '=', 'purchases.purchase_id')
-                    ->join('products', 'product_purchases.product_id', '=', 'products.product_id')->groupBy('purchases.purchase_id')->count();
-                    $jumlah_pesanan_berhasil_telah_dibayar = $count_status5;
-                ?>
-
                 @endforeach
             @endforeach
         </div><!-- .End .tab-pane -->
